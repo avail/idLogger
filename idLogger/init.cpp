78b3565e4b44099b9b85ad130dbe622560d8e06f
avail <avail@pomf.se>
2017-07-07 01:41:48 +0200

    load ini file on init

--- idLogger/init.cpp
+++ idLogger/init.cpp
@@ -1,9 +1,20 @@
 #include "idLogger.h"
 
+linb::ini config;
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		
+		if (!config.load_file("idLogger-config.ini"))
+		{
+			MessageBoxA(NULL, "Failed to open idLogger-config.ini", "Oshibka", MB_OK);
+			std::_Exit(0);
+		}
+
 		InitFunction::RunFunctions();
+	}
 
 	return TRUE; // false
 }
