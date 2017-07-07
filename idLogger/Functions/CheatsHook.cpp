724b0ab0ac288a6adf24b786865406ea864e2403
avail <avail@pomf.se>
2017-07-07 05:04:29 +0200

    skip network init, makes game initialise *a lot* faster

--- idLogger/Functions/CheatsHook.cpp
+++ idLogger/Functions/CheatsHook.cpp
@@ -1,9 +1,17 @@
 #include "../idLogger.h"
 
+int hookedConnectToNet()
+{
+	return 0;
+}
+
 static InitFunction cheatsFunc([]()
 {
 	// fullspec
 	// dumb tired hooking attempt that never worked ;p
 	/*injector::MakeNOP(0x4CCE20, 2);
 	((int(__thiscall*)(int))0x4CCE00)(0);*/
+
+	// fail network init instantly
+	injector::MakeCALL(0x9B1C70 + 0x3B8, hookedConnectToNet);
 });
