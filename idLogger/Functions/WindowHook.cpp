360469d36426ff7951bf69eb5aec4d82657534b0
avail <avail@pomf.se>
2017-07-24 21:35:16 +0200

    ability to disable resolution patches, borderless mode experiments

--- idLogger/Functions/WindowHook.cpp
+++ idLogger/Functions/WindowHook.cpp
@@ -1,6 +1,6 @@
 #include "../idLogger.h"
 
-DWORD g_windowStyle = WS_VISIBLE | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+DWORD g_windowStyle;
 
 FILE* windowLog;
 
@@ -11,6 +11,7 @@ int g_width = 0;
 int g_height = 0;
 
 bool enableWindowLog = false;
+bool enableBorderless = false;
 
 HWND hwndWindowA;
 HWND hwndWindowW;
@@ -22,12 +23,6 @@ HWND WINAPI CreateWindowExAHk(DWORD dwExStyle, LPCSTR lpClassName, LPCSTR lpWind
 		fprintf(windowLog, "CreateWindowExAHooked: lpClassName %s lpWindowName %s x %i y %i nWidth %i nHeight %i dwStyle %p\n", lpClassName, lpWindowName, x, y, nWidth, nHeight, dwStyle);
 		fflush(windowLog);
 	}
-
-	int rx, ry;
-	GetDesktopResolution(rx, ry);
-
-	g_x = (rx / 2) - (nWidth / 2);
-	g_y = (ry / 2) - (nHeight / 2);
 
 	if (lpWindowName)
 	{
@@ -51,12 +46,6 @@ HWND WINAPI CreateWindowExWHk(DWORD dwExStyle, LPCWSTR lpClassName, LPCWSTR lpWi
 		fprintf(windowLog, "CreateWindowExWHooked: lpClassName %s lpWindowName %s x %i y %i nWidth %i nHeight %i dwStyle %p\n", lpClassName, lpWindowName, x, y, nWidth, nHeight, dwStyle);
 		fflush(windowLog);
 	}
-
-	int rx, ry;
-	GetDesktopResolution(rx, ry);
-
-	g_x = (rx / 2) - (nWidth / 2);
-	g_y = (ry / 2) - (nHeight / 2);
 
 	if (lpWindowName)
 	{
@@ -90,7 +79,7 @@ BOOL WINAPI SetWindowPosHk(HWND hWnd, HWND hWndInsertAfter, int X, int Y, int cx
 {
 	if (enableWindowLog)
 	{
-		fprintf(windowLog, "SetWindowPosHooked: hWnd %p X %i Y %i cx %i cy %i\n", hWnd, X, Y, cx, cy);
+		fprintf(windowLog, "SetWindowPosHooked: hWnd %p X %i Y %i cx %i cy %i uFlags %i\n", hWnd, X, Y, cx, cy);
 		fflush(windowLog);
 	}
 
@@ -113,13 +102,23 @@ LONG __stdcall ChangeDisplaySettingsHk(DEVMODEA *lpDevMode, DWORD dwFlags)
 
 	lpDevMode = NULL; // retain original changes instead of applying Initial D's modified values
 
-	return ChangeDisplaySettingsA(lpDevMode, dwFlags);
+	if (enableBorderless)
+		return DISP_CHANGE_SUCCESSFUL;
+	else
+		return ChangeDisplaySettingsA(lpDevMode, dwFlags);
+}
+
+BOOL WINAPI UpdateWindowHk(HWND hWnd)
+{
+	return true;
 }
 
 static InitFunction windowHookFunc([]()
 {
 	bool enableWindowed = ToBool(config["WindowHooks"]["EnableWindowedModeHook"]);
+	enableBorderless = ToBool(config["WindowHooks"]["BorderlessModeHook"]);
 	enableWindowLog = ToBool(config["Logging"]["EnableWindowLog"]);
+	bool enableResolutionPatches = ToBool(config["Logging"]["EnableResolutionPatches"]);
 
 	linb::ini gameConfig;
 	gameConfig.read_file("../InitialD.ini");
@@ -132,55 +131,86 @@ static InitFunction windowHookFunc([]()
 		windowLog = fopen("idLogger-window.txt", "w");
 	}
 
-	if (enableWindowed)
+	if (enableBorderless)
+	{
+		// borderless windowed mode
+		g_windowStyle = WS_POPUP | WS_VISIBLE | WS_SYSMENU;
+		GetDesktopResolution(g_width, g_height);
+
+		g_x = 0;
+		g_y = 0;
+	}
+	else
+	{
+		// regular windowed mode
+		g_windowStyle = WS_VISIBLE | WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+
+		int rx, ry;
+		GetDesktopResolution(rx, ry);
+
+		g_x = (rx / 2) - (g_width / 2);
+		g_y = (ry / 2) - (g_height / 2);
+	}
+
+	if (enableWindowed || enableBorderless)
 	{
 		*(HWND*)0xDBB774 = (HWND)CreateWindowExAHk;
 		*(HWND*)0xDBB754 = (HWND)CreateWindowExWHk;
 		*(BOOL*)0xDBB7A4 = (BOOL)AdjustWindowRectHk;
 		*(BOOL*)0xDBB7A8 = (BOOL)SetWindowPosHk;
 		*(LONG*)0xDBB7BC = (LONG)ChangeDisplaySettingsHk;
+		*(BOOL*)0xDBB7B4 = (BOOL)UpdateWindowHk;
 	}
 
-	// height
-	injector::WriteMemory(0x768A20 + 0x24E + 1, g_height, true);
-	injector::WriteMemory(0x768DC5, g_height, true);
-	injector::WriteMemory(0x768A20 + 0xBB7 + 1, g_height, true);
-	injector::WriteMemory(0x76C8A8, g_height, true);
-	injector::WriteMemory(0x76D129, g_height, true);
-	injector::WriteMemory(0x7B4400 + 0x44 + 1, g_height, true);
-	injector::WriteMemory(0x7B4640 + 0x5B + 1, g_height, true);
-	injector::WriteMemory(0x7B4640 + 0x71 + 1, g_height, true);
-	injector::WriteMemory(0x83D82E, g_height, true);
-	injector::WriteMemory(0x865780 + 0x44 + 1, g_height, true);
-	injector::WriteMemory(0x86D271, g_height, true);
-	injector::WriteMemory(0x87EE20, g_height, true);
+	if (enableResolutionPatches)
+	{
+		// height
+		injector::WriteMemory(0x768A20 + 0x24E + 1, g_height, true);
+		injector::WriteMemory(0x768DC5, g_height, true);
+		injector::WriteMemory(0x768A20 + 0xBB7 + 1, g_height, true);
+		injector::WriteMemory(0x76C8A8, g_height, true);
+		injector::WriteMemory(0x76D129, g_height, true);
+		injector::WriteMemory(0x7B4400 + 0x44 + 1, g_height, true);
+		injector::WriteMemory(0x7B4640 + 0x5B + 1, g_height, true);
+		injector::WriteMemory(0x7B4640 + 0x71 + 1, g_height, true);
+		injector::WriteMemory(0x83D82E, g_height, true);
+		injector::WriteMemory(0x865780 + 0x44 + 1, g_height, true);
+		injector::WriteMemory(0x86D271, g_height, true);
+		injector::WriteMemory(0x87EE20, g_height, true);
 
-	// width
-	injector::WriteMemory(0x768A20 + 0x253 + 1, g_width, true);
-	injector::WriteMemory(0x768DBA, g_width, true);
-	injector::WriteMemory(0x768A20 + 0xBBC + 1, g_width, true);
-	injector::WriteMemory(0x76C8A3, g_width, true);
-	injector::WriteMemory(0x76D122, g_width, true);
-	injector::WriteMemory(0x7B4400 + 0x49 + 1, g_width, true);
-	injector::WriteMemory(0x7B4640 + 0x60 + 1, g_width, true);
-	injector::WriteMemory(0x7B4640 + 0x76 + 1, g_width, true);
-	injector::WriteMemory(0x83D825, g_width, true);
-	injector::WriteMemory(0x865780 + 0x49 + 1, g_width, true);
-	injector::WriteMemory(0x86D269, g_width, true);
-	injector::WriteMemory(0x87EE18, g_width, true);
+		// width
+		injector::WriteMemory(0x768A20 + 0x253 + 1, g_width, true);
+		injector::WriteMemory(0x768DBA, g_width, true);
+		injector::WriteMemory(0x768A20 + 0xBBC + 1, g_width, true);
+		injector::WriteMemory(0x76C8A3, g_width, true);
+		injector::WriteMemory(0x76D122, g_width, true);
+		injector::WriteMemory(0x7B4400 + 0x49 + 1, g_width, true);
+		injector::WriteMemory(0x7B4640 + 0x60 + 1, g_width, true);
+		injector::WriteMemory(0x7B4640 + 0x76 + 1, g_width, true);
+		injector::WriteMemory(0x83D825, g_width, true);
+		injector::WriteMemory(0x865780 + 0x49 + 1, g_width, true);
+		injector::WriteMemory(0x86D269, g_width, true);
+		injector::WriteMemory(0x87EE18, g_width, true);
 
-	// remove limits in reading resolution from configuration
-	// less than 640, bigger than 1600 (width)
-	injector::MakeNOP(0x9B4C30 + 0x133, 2);
-	injector::WriteMemory<BYTE>(0x9B4C30 + 0x13A, 0xEB, true);
-	
-	// some resolutions are not divisble by 8, so skip that check too
-	injector::WriteMemory<BYTE>(0x9B4C30 + 0x144, 0xEB, true);
+		// remove limits in reading resolution from configuration
+		// less than 640, bigger than 1600 (width)
+		injector::MakeNOP(0x9B4C30 + 0x133, 2);
+		injector::WriteMemory<BYTE>(0x9B4C30 + 0x13A, 0xEB, true);
 
-	// less than 400, bigger than 1200 (height)
-	injector::MakeNOP(0x9B4C30 + 0x16F, 2);
-	injector::WriteMemory<BYTE>(0x9B4C30 + 0x177, 0xEB, true);
+		// some resolutions are not divisble by 8, so skip that check too
+		injector::WriteMemory<BYTE>(0x9B4C30 + 0x144, 0xEB, true);
 
-	// some resolutions are not divisble by 8, so skip that check too
-	injector::WriteMemory<BYTE>(0x9B4C30 + 0x181, 0xEB, true);
+		// less than 400, bigger than 1200 (height)
+		injector::MakeNOP(0x9B4C30 + 0x16F, 2);
+		injector::WriteMemory<BYTE>(0x9B4C30 + 0x177, 0xEB, true);
+
+		// some resolutions are not divisble by 8, so skip that check too
+		injector::WriteMemory<BYTE>(0x9B4C30 + 0x181, 0xEB, true);
+	}
+
+	if (enableBorderless)
+	{
+		// set ppfd flag to allow for composite
+		injector::WriteMemory<WORD>(0xAADB54, 0x9025, true);
+	}
 });
